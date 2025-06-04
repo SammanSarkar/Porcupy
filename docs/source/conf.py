7f48a78e4342f66cfda0d384e11e7c86902b6c47
Samman Sarkar <sarkar.samman4231@gmail.com>
2025-06-04 13:40:39 +0530

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,24 +1,123 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+from datetime import datetime
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath('../../'))
+
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 project = 'Porcupy'
-copyright = '2025, Samman Sarkar'
+copyright = f'{datetime.now().year}, Samman Sarkar'
 author = 'Samman Sarkar'
+version = '0.2.0'
 release = '0.2.0'
-version = '0.2'  # optional but good practice
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
-    'myst_parser',          # Support for Markdown
-    'sphinx.ext.autodoc',   # For docstrings (if any)
-    'sphinx.ext.napoleon',  # For Google-style docstrings
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.intersphinx',
+    'myst_parser',
+    'sphinx_copybutton',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
 ]
 
-# Allow both .rst and .md
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
 
-exclude_patterns = []
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'alabaster'
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+
+# -- Extension configuration -------------------------------------------------
+
+# autodoc configuration
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__',
+    'show-inheritance': True,
+}
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# Intersphinx configuration
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+}
+
+# MyST configuration
+myst_enable_extensions = [
+    'dollarmath',
+    'amsmath',
+    'deflist',
+    'colon_fence',
+    'html_admonition',
+    'html_image',
+]
+
+# Copy button configuration
+copybutton_prompt_text = r'>>> |\.\.\. |\$ |In \[\d+\]: |\s*\.\.\.: |\s*\$ |\s*%\w+ |\s*\w+>' 
+copybutton_prompt_is_regexp = True
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,17 +1,50 @@
-.. Porcupy documentation master file, created by
-   sphinx-quickstart on Wed Jun  4 13:11:12 2025.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. Porcupy documentation master file
 
-Porcupy documentation
-=====================
+Porcupy: Crested Porcupine Optimizer
+===================================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+.. image:: /resources/img/logo.png
+   :alt: Porcupy Logo
+   :align: right
+   :width: 200
 
+A Python implementation of the Crested Porcupine Optimizer (CPO) algorithm, featuring GPU acceleration and comprehensive visualization tools.
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: Getting Started
 
+   getting_started/quickstart
+   getting_started/installation
+   getting_started/configuration
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User Guide
+
+   user_guide/basic_usage
+   user_guide/advanced_features
+   user_guide/visualization
+   user_guide/gpu_acceleration
+   user_guide/faq
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Examples
+
+   examples/basic_optimization
+   examples/advanced_optimization
+   examples/real_world_applications
+
+.. toctree::
+   :maxdepth: 2
+   :caption: API Reference
+
+   api_reference/api_reference
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
